6c24141de5b5e7c71bf786561d4414a8451ea1ae
Campbell <121409172+DuskieWhy@users.noreply.github.com>
2026-03-27 19:47:44 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -24,7 +24,7 @@ class Main extends Sprite
 			fps: 60,
 			skipSplash: #if debug true #else false #end,
 			startFullScreen: false,
-			initialState: funkin.states.editors.WIPNoteSkinEditor
+			initialState: funkin.states.TitleState
 		};
 		
 	static function __init__()
